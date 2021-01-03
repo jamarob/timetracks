243f195fd88443a860be64f98cae80c15256b984
Jan Robert <janmartinrobert@gmail.com>
2021-01-03 22:53:54 +0100

    Add logo to header

--- src/common/AppHeader.tsx
+++ src/common/AppHeader.tsx
@@ -1,41 +1,52 @@
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components/macro'
 
+import { Logo } from '../assets'
+
 export default function AppHeader() {
   return (
     <Header>
-      <h1>TimeTracks</h1>
-      <nav>
-        <NavLink to="/wip">WIP</NavLink>
-        <NavLink to="/timesheet">Timesheet</NavLink>
-      </nav>
+      <Heading>
+        <Logo />
+        <h1>TimeTracks</h1>
+      </Heading>
+      <Nav>
+        <PageLink to="/wip">WIP</PageLink>
+        <PageLink to="/timesheet">Timesheet</PageLink>
+      </Nav>
     </Header>
   )
 }
 
 const Header = styled.header`
-  padding: 16px;
+  padding: 8px 16px;
   display: grid;
-  grid-gap: 16px;
+  grid-gap: 8px;
+`
+
+const Heading = styled.section`
+  display: flex;
+  align-items: baseline;
 
   h1 {
     margin: 0;
+    margin-left: 8px;
   }
+`
 
-  nav {
-    display: grid;
-    grid-auto-columns: min-content;
-    grid-auto-flow: column;
-    grid-gap: 16px;
-  }
+const Nav = styled.nav`
+  display: grid;
+  grid-auto-columns: min-content;
+  grid-auto-flow: column;
+  grid-gap: 16px;
+`
 
-  a {
-    text-decoration: none;
-    color: #eee;
-    border-bottom: 2px solid transparent;
-  }
+const PageLink = styled(NavLink)`
+  text-decoration: none;
+  color: #eee;
+  border-bottom: 2px solid transparent;
 
-  a.active {
+  &.active {
     font-weight: 600;
     border-color: deeppink;
   }
